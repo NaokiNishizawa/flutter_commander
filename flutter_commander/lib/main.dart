5cbf06165e0f3a307297a10531fbe44f2e2ee0db
Naoki Nishizawa <mino24zawawork@gmail.com>
2023-06-18 02:10:48 +0900

    [test] do shell command from app

--- flutter_commander/lib/main.dart
+++ flutter_commander/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:process_run/shell.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,16 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Commander',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: const MyHomePage(title: 'Flutter Commander Home Page'),
     );
@@ -48,8 +40,13 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            ElevatedButton(
+              onPressed: () async {
+                // TODO do command
+                var shell = Shell();
+                await shell.run('''flutter pub get''');
+              },
+              child: const Text('pub get'),
             ),
           ],
         ),
