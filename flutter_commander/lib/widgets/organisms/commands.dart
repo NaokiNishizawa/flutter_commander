45d9823cd2f1e1253441f419e2b132dd61dc73ea
Naoki Nishizawa <mino24zawawork@gmail.com>
2023-06-18 06:13:04 +0900

    add command page action

--- flutter_commander/lib/widgets/organisms/commands.dart
+++ flutter_commander/lib/widgets/organisms/commands.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_commander/enums/command_type.dart';
-import 'package:flutter_commander/main.dart';
 import 'package:flutter_commander/models/command_info_model.dart';
+import 'package:flutter_commander/widgets/organisms/result_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class Commands extends ConsumerStatefulWidget {
+class Commands extends StatefulWidget {
   const Commands({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _CommandsState();
+  State<StatefulWidget> createState() => _CommandsState();
 }
 
-class _CommandsState extends ConsumerState<Commands> {
+class _CommandsState extends State<Commands> {
   @override
   Widget build(BuildContext context) {
-    final projectInfoModel = ref.read(projectInfoProvider);
-
     // FIXME 本当はFileなどから読み込めるようにすること ここから
     final doctorCmd = CommandInfoModel();
     doctorCmd.type = CommandType.doNotNeedAnything;
@@ -27,7 +25,7 @@ class _CommandsState extends ConsumerState<Commands> {
 
     final pubAddCmd = CommandInfoModel();
     pubAddCmd.type = CommandType.inputArgument;
-    pubAddCmd.script = 'flutter pub add ';
+    pubAddCmd.script = 'flutter pub add';
 
     final List<CommandInfoModel> commandInfoModelList = [
       doctorCmd,
@@ -39,10 +37,10 @@ class _CommandsState extends ConsumerState<Commands> {
     return ListView.builder(
       itemCount: commandInfoModelList.length,
       itemBuilder: (BuildContext context, int index) {
-        final widget =
-            commandInfoModelList[index].type == CommandType.doNotNeedAnything
-                ? NoArgumentCmdForm(model: commandInfoModelList[index])
-                : InputArgumentCmdForm(model: commandInfoModelList[index]);
+        final infoModel = commandInfoModelList[index];
+        final widget = infoModel.type == CommandType.doNotNeedAnything
+            ? NoArgumentCmdForm(model: infoModel)
+            : InputArgumentCmdForm(model: infoModel);
 
         return Card(
           child: widget,
@@ -52,50 +50,70 @@ class _CommandsState extends ConsumerState<Commands> {
   }
 }
 
-class NoArgumentCmdForm extends StatelessWidget {
+class NoArgumentCmdForm extends ConsumerWidget {
   const NoArgumentCmdForm({super.key, required this.model});
 
   final CommandInfoModel model;
 
   @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Expanded(
-          child: Text(model.script),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return InkWell(
+      onTap: () {
+        ref.read(scriptProvider.notifier).update((state) => model.script);
+      },
+      child: SizedBox(
+        height: 48,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Text(model.script),
+            ),
+            const Icon(Icons.directions_run),
+          ],
         ),
-        const Icon(Icons.directions_run),
-      ],
+      ),
     );
   }
 }
 
-class InputArgumentCmdForm extends StatelessWidget {
+class InputArgumentCmdForm extends ConsumerWidget {
   InputArgumentCmdForm({super.key, required this.model});
 
   final CommandInfoModel model;
   final _argumentTextController = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Expanded(
-            child: Row(
-          children: [
-            Text(model.script),
-            Expanded(
-              child: TextFormField(
-                controller: _argumentTextController,
-                decoration: const InputDecoration(hintText: '追加するパッケージ名'),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return InkWell(
+      onTap: () {
+        final packageName = _argumentTextController.text.toString();
+        if (packageName.isEmpty) {
+          // TODO 画面にダイアログを表示すること
+          return;
+        }
+
+        final script = '${model.script} $packageName';
+        ref.read(scriptProvider.notifier).update((state) => script);
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+              child: Row(
+            children: [
+              Text(model.script),
+              Expanded(
+                child: TextFormField(
+                  controller: _argumentTextController,
+                  decoration: const InputDecoration(hintText: '追加するパッケージ名'),
+                ),
               ),
-            ),
-          ],
-        )),
-        const Icon(Icons.directions_run),
-      ],
+            ],
+          )),
+          const Icon(Icons.directions_run),
+        ],
+      ),
     );
   }
 }
